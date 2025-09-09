6c99f6b3e914d087e7ad9236fd44a8cb5c5c87f6
D <fardee.tat@gmail.com>
2025-09-10 00:10:41 +0700

    refactor: style placeholder and config route to redirect '/tns-crud/test' path for first page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TnsCrudTestComponent } from './pages/tns-crud-test/tns-crud-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tns-crud-test',
+    pathMatch: 'full',
+  },
+  { path: 'tns-crud-test', component: TnsCrudTestComponent },
+];
---
+++ frontend/src/app/pages/tns-crud-test/tns-crud-test.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { DepartmentFormComponent } from '../../features/department/department-form/department-form.component';
+import { DepartmentListComponent } from '../../features/department/department-list/department-list.component';
+import { UserFormComponent } from '../../features/user/user-form/user-form.component';
+import { UserListComponent } from '../../features/user/user-list/user-list.component';
+import { CommonModule } from '@angular/common';
+import { IDepartment } from '../../shared/model/department';
+import { IUser } from '../../shared/model/user';
+import { DepartmentService } from '../../shared/services/department.service';
+import { UserService } from '../../shared/services/user.service';
+
+@Component({
+  selector: 'app-tns-crud-test',
+  imports: [
+    DepartmentFormComponent,
+    DepartmentListComponent,
+    UserFormComponent,
+    UserListComponent,
+    CommonModule,
+  ],
+  templateUrl: './tns-crud-test.component.html',
+})
+export class TnsCrudTestComponent implements OnInit {
+isFormOpen = false;
+  currentView: 'department' | 'user' = 'department';
+  editDepartment: IDepartment | null = null;
+  editUser: IUser | null = null;
+
+  departments: IDepartment[] = [];
+  users: IUser[] = [];
+
+  constructor(
+    private departmentService: DepartmentService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadDataDepartments();
+    this.loadDataUsers();
+  }
+
+  loadDataDepartments() {
+    this.departmentService.getAllDepartment().subscribe({
+      next: (response) => {
+        this.departments = response;
+      },
+    });
+  }
+
+  loadDataUsers() {
+    this.userService.getAllUser().subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+    });
+  }
+
+  switchView(type: 'department' | 'user') {
+    this.currentView = type;
+    this.isFormOpen = false;
+  }
+
+  handleOpenForm() {
+    this.isFormOpen = true;
+  }
+
+  handleCloseForm() {
+    this.editDepartment = null;
+    this.editUser = null;
+    this.isFormOpen = false;
+  }
+
+  handleEditDepartment(department: IDepartment) {
+    this.editDepartment = department;
+    this.isFormOpen = true;
+  }
+
+  handleEditUser(user: IUser) {
+    this.editUser = user;
+    this.isFormOpen = true;
+  }
+}
